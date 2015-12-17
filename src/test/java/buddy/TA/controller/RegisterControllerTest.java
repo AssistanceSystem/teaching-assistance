e21d642e8277a8096d978532461be665249af513
Liu Lu <lu.liu@rea-group.com>
2015-12-18 00:40:25 +0800

    fix test style

--- src/test/java/buddy/TA/controller/RegisterControllerTest.java
+++ src/test/java/buddy/TA/controller/RegisterControllerTest.java
@@ -1,12 +1,8 @@
 package buddy.TA.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-
 import buddy.TA.TeachingAssistanceApplication;
 import buddy.TA.entity.User;
 import buddy.TA.service.UserService;
-import net.sf.json.JSON;
 import net.sf.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
@@ -19,12 +15,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import static org.junit.Assert.assertEquals;
-/**
- * Created by zhihu on 15/12/15.
- */
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TeachingAssistanceApplication.class)
 public class RegisterControllerTest {
@@ -38,90 +32,87 @@ public class RegisterControllerTest {
     private  MockMvc mockMvc;
 
     @Before
-    public void setup(){
+    public void setup() {
         mockMvc = standaloneSetup(registerController).build();
     }
 
-
     @Test
     public void checkUsedLoginNameTest() throws Exception {
-        String json ="{\"loginName\":\"max\"}";
+        String json = "{\"loginName\":\"max\"}";
         MvcResult result = mockMvc.perform(post("/register/check").characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json.getBytes()))
                 .andReturn();
         String response = result.getResponse().getContentAsString();
         JSONObject data = JSONObject.fromObject(response);
-        assertEquals("failed",data.get("message"));
+        assertEquals("failed", data.get("message"));
     }
 
     @Test
     public void checkUnusedLoginNameTest() throws Exception {
-        String json ="{\"loginName\":\"tom\"}";
+        String json = "{\"loginName\":\"tom\"}";
         MvcResult result = mockMvc.perform(post("/register/check").characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json.getBytes()))
                 .andReturn();
         String response = result.getResponse().getContentAsString();
         JSONObject data = JSONObject.fromObject(response);
-        assertEquals("success",data.get("message"));
+        assertEquals("success", data.get("message"));
     }
 
     @Test
     public void successCreateUserTest() throws Exception {
-        String json ="{\"loginName\":\"Jim@163.com\",\"name\":\"user1\",\"password\":\"123456\"}";
+        String json = "{\"loginName\":\"Jim@163.com\",\"name\":\"user1\",\"password\":\"123456\"}";
         MvcResult result = mockMvc.perform(post("/register/create").characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json.getBytes()))
                 .andReturn();
         String response = result.getResponse().getContentAsString();
         JSONObject data = JSONObject.fromObject(response);
-        assertEquals("success",data.get("message"));
+        assertEquals("success", data.get("message"));
     }
 
     @Test
     public void loginNameErrorCreateUserTest() throws Exception {
-        String json ="{\"loginName\":\"Jim\",\"name\":\"user1\",\"password\":\"123456\"}";
+        String json = "{\"loginName\":\"Jim\",\"name\":\"user1\",\"password\":\"123456\"}";
         MvcResult result = mockMvc.perform(post("/register/create").characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json.getBytes()))
                 .andReturn();
         String response = result.getResponse().getContentAsString();
         JSONObject data = JSONObject.fromObject(response);
-        assertEquals("loginName illegal",data.get("message"));
+        assertEquals("loginName illegal", data.get("message"));
     }
 
     @Test
     public void passwordShortErrorCreateUserTest() throws Exception {
-        String json ="{\"loginName\":\"Jim@163.com\",\"name\":\"user1\",\"password\":\"1234\"}";
+        String json = "{\"loginName\":\"Jim@163.com\",\"name\":\"user1\",\"password\":\"1234\"}";
         MvcResult result = mockMvc.perform(post("/register/create").characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json.getBytes()))
                 .andReturn();
         String response = result.getResponse().getContentAsString();
         JSONObject data = JSONObject.fromObject(response);
-        assertEquals("password illegal",data.get("message"));
+        assertEquals("password illegal", data.get("message"));
     }
 
     @Test
     public void passwordLongErrorCreateUserTest() throws Exception {
-        String json ="{\"loginName\":\"Jim@163.com\",\"name\":\"user1\",\"password\":\"12345678901234567890\"}";
+        String json = "{\"loginName\":\"Jim@163.com\",\"name\":\"user1\",\"password\":\"12345678901234567890\"}";
         MvcResult result = mockMvc.perform(post("/register/create").characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json.getBytes()))
                 .andReturn();
         String response = result.getResponse().getContentAsString();
         JSONObject data = JSONObject.fromObject(response);
-        assertEquals("password illegal",data.get("message"));
+        assertEquals("password illegal", data.get("message"));
     }
 
     @After
-    public void deleteData(){
+    public void deleteData() {
         User user = userService.findByLoginName("Jim@163.com");
-        if (user != null){
+        if (user != null) {
             userService.deleteUser(user.getId());
         }
     }
-
-
 }
